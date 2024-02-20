c389443523628dba16113e9bf241a5b890505272
OXYGENBOTTLE_MBP <810104157@qq.com>
2024-02-21 00:31:14 +0800

    设置手机状态栏

---
+++ pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main"></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      title: "Hello",
+      inited: false,
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+}
+</style>
